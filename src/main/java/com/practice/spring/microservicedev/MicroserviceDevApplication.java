4a9a88d4df64286a506d07c860b9cbec7022107d
sreenadh1543@gmail.com <sreenadh1543@gmail.com>
2020-07-10 23:00:40 +0200

    project setup : initial commit

---
+++ src/main/java/com/practice/spring/microservicedev/MicroserviceDevApplication.java
@@ -0,0 +1,13 @@
+package com.practice.spring.microservicedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDevApplication.class, args);
+	}
+
+}
